8a0ecaa5e7a1243bfa3b4b6b4a7017533a4711a8
Ashtankar, Kamalakar <kamalakar.ashtankar@willistowerswatson.com>
2018-11-22 17:56:37 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AuthService } from 'src/app/services/authtoken.service';
+import { PowerBiService } from 'src/app/services/powerbi.service';
+import { WindowRef } from 'src/app/services/WindowRef';
+import { AuthenticationInterceptor } from 'src/app/services/authentication.interceptor';
+//https://blog.angular-university.io/angular-service-worker/
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [AuthService,PowerBiService,WindowRef,{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthenticationInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
